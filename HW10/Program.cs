7d3d94c7a25cb3a7b925b6535f0069048c33c61b
Alexandra Camarena <alexandracamarena@ou.edu>
2024-04-30 18:38:21 -0500

    Add project files.

---
+++ HW10/Program.cs
@@ -0,0 +1,124 @@
+﻿// MIS 3013 001
+// HW 10
+// Alexandra Camarena
+// 113568154
+
+//Console.WriteLine("HW10");
+
+using HW10;
+
+string menuStr = """
+*********************************
+Please choose from the options:
+1: Add a new receipt
+2: Print all receipts
+3: Print all receipts of one customer
+4: Print the highest total receipt
+5: Print the average grand total
+Press other keys to exit
+**********************************
+""";
+
+List<Receipt>receiptList = new List<Receipt>();
+
+while (true)
+{
+    Console.WriteLine(menuStr);
+
+    Console.WriteLine();
+
+    Console.Write($"Enter an option: ");
+    string userChoiceStr = Console.ReadLine();
+
+    if (userChoiceStr == "1")
+    {
+        Console.WriteLine("\nAdd a new receipt\n".ToUpper());
+
+        Console.Write($"Input the customer ID: ");
+        string idStr = Console.ReadLine();
+
+        Console.Write($"Input N of cogs: ");
+        string nCogsStr = Console.ReadLine();
+        int nCogsInt = Convert.ToInt32(nCogsStr);
+
+        Console.Write($"Input N of gears: ");
+        string nGearsStr = Console.ReadLine();
+        int nGearsInt = Convert.ToInt32(nGearsStr);
+
+        Receipt r = new Receipt();
+
+        r.CustomerID = idStr;
+        r.CogQuantity = nCogsInt;
+        r.GearQuantity = nGearsInt;
+
+        r.CalculateGrandTotal();
+
+        Console.WriteLine();
+
+        r.PrintReceipt();
+
+        Console.WriteLine();
+
+        receiptList.Add(r);
+    }
+    else if (userChoiceStr == "2")
+    {
+        Console.WriteLine("\nAll Receipts\n".ToUpper());
+        for (int i = 0; i <= receiptList.Count - 1; i = i + 1)
+        {
+            Receipt r = receiptList[i];
+            r.PrintReceipt();
+        }
+    }
+    else if (userChoiceStr == "3")
+    {
+        Console.WriteLine("\nAll Receipts for one customer\n");
+
+        Console.Write("Input a customer ID: ");
+        string idStr = Console.ReadLine();
+
+        for (int i = 0; i <= receiptList.Count - 1; i = i + 1)
+        {
+            Receipt r = receiptList[i];
+            if (r.CustomerID==idStr)
+            {
+                r.PrintReceipt();
+            }
+        }
+    }
+    else if (userChoiceStr == "4")
+    {
+        Console.WriteLine("\nReceipt with highest grand total\n".ToUpper());
+
+        Receipt hr = receiptList[0];
+
+        for (int i = 0; i <= receiptList.Count - 1; i = i + 1)
+        {
+            Receipt r = receiptList[i];
+            if (r.GrandTotal > hr.GrandTotal)
+            {
+                hr = r;
+            }
+        }
+        hr.PrintReceipt();
+    }
+    else if (userChoiceStr == "5")
+    {
+        Console.WriteLine("\nAverage grand total\n");
+
+        double grandTotalSum = 0;
+
+        for (int i = 0; i <= receiptList.Count - 1; i = i + 1)
+        { 
+            grandTotalSum = grandTotalSum + receiptList[i].GrandTotal;
+        }
+
+        double grandAverage = grandTotalSum / receiptList.Count;
+        Console.WriteLine($"Average: {grandAverage:C2}");
+    }
+    else
+    {
+        Console.WriteLine("Thank you and goodbye!");
+        break;    
+    }
+}
---
+++ HW10/Receipt.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HW10// folder name: HW10
+{
+    public class Receipt
+    {
+        public string CustomerID;
+        public int CogQuantity;
+        public int GearQuantity;
+        public DateTime SaleDate;
+        public double Subtotal;
+        public double SalesTax;
+        public double GrandTotal;
+
+        private double SalesTaxRate;
+        private double CogPrice;
+        private double GearPrice;
+
+        // constructor functions
+        public Receipt() 
+        {
+            this.CustomerID = "";
+            this.CogQuantity = 0;
+            this.GearQuantity = 0;
+
+            this.SaleDate = DateTime.Now;
+            this.Subtotal = 0;
+            this.SalesTaxRate = 0;
+            this.GrandTotal = 0;
+
+            this.SalesTaxRate = 0.08;
+            this.CogPrice = 80.00;
+            this.GearPrice = 250.00;
+        }
+        public Receipt(string id, int cog, int gear)
+        { 
+            this.CustomerID = id;
+            this.CogQuantity = cog;
+            this.GearQuantity = gear;
+
+            this.SaleDate = DateTime.Now;
+            this.Subtotal = 0;
+            this.SalesTaxRate = 0;
+            this.GrandTotal = 0;
+
+            this.SalesTaxRate = 0.08;
+            this.CogPrice = 80.00;
+            this.GearPrice = 250.00;
+        }
+        private double CalculateSubtotal()
+        {
+            this.Subtotal = this.CogPrice * this.CogQuantity + this.GearPrice * this.GearQuantity;
+            return this.Subtotal;
+        }
+        private double CalculateSalesTax()
+        {
+            this.SalesTax = this.Subtotal*this.SalesTaxRate;
+            return this.SalesTax;
+        }
+
+        public double CalculateGrandTotal()
+        {
+            this.CalculateSubtotal();
+            this.CalculateSalesTax();
+
+            this.GrandTotal = this.Subtotal + this.SalesTax;
+            
+            return this.GrandTotal;
+        }
+
+        public void PrintReceipt()
+        { 
+            Console.WriteLine("======================================");
+            Console.WriteLine($"Receipt".ToUpper());
+            Console.WriteLine($"Customer ID: {this.CustomerID}");
+            Console.WriteLine($"# of cogs: {this.CogQuantity}");
+            Console.WriteLine($"# of gears: {this.GearQuantity}");
+            Console.WriteLine($"Subtotal: {this.Subtotal:C2}");
+            Console.WriteLine($"Sales Tax: {this.SalesTax:C2}");
+            Console.WriteLine($"Grand Total: {this.GrandTotal:C2}");
+            Console.WriteLine($"Time: {this.SaleDate}");
+            Console.WriteLine("======================================");
+        }
+    }
+}
